5be25917956929c307a5451d35b174a706e37a32
pawunder1992 <pavelkryvutsa@gmail.com>
2026-03-29 19:20:59 +0300

    Changed parameters to fixed value

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 class Animal:
-    def __init__(self, name: str, appetite: int,
+    def __init__(self,
+                 name: str,
+                 appetite: int,
                  is_hungry: bool = True) -> None:
         self.name = name
         self.appetite = appetite
@@ -9,27 +11,24 @@ class Animal:
         print(f"Hello, I'm {self.name}")
 
     def feed(self) -> int:
-        if self.is_hungry:
+        if self.is_hungry :
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
             return self.appetite
-        self.appetite = 0
-        return self.appetite
+        return 0
 
 
 class Cat(Animal):
-    def __init__(self, name: str, is_hungry: bool = True,
-                 appetite: int = 3) -> None:
-        super().__init__(name, appetite, is_hungry)
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name=name, appetite=3, is_hungry=is_hungry)
 
     def catch_mouse(self) -> None:
         print("The hunt began!")
 
 
 class Dog(Animal):
-    def __init__(self, name: str, is_hungry: bool = True,
-                 appetite: int = 7) -> None:
-        super().__init__(name, appetite, is_hungry)
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name=name, appetite=7, is_hungry=is_hungry)
 
     def bring_slippers(self) -> None:
         print("The slippers delivered!")
